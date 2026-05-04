72e787d64e131bb95273dc0e94597798a253ec84
Jaypatel1511 <thejaypatel1511@gmail.com>
2026-05-04 12:40:55 -0500

    fix: restore correct description and version to 0.1.2, remove incorrect CHANGELOG

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cdfidata",
-    version="0.1.6",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "pandas>=1.4.0",
